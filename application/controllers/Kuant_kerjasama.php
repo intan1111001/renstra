6475d06c0f5479296cbfb5d181b7e34059f4b2c5
Ahmad Yusuf <>
2019-08-26 08:18:26 +0700

    kuantitatif kerjasama

---
+++ application/controllers/Kuant_kerjasama.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kuant_kerjasama extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	// public function index()
+	// {
+	// 	$this->load->view('welcome_message');
+	// }
+
+	function __construct() 
+    { 
+	    parent::__construct(); 
+	    $this->load->model('Survei_model'); 
+	    $this->load->model('Elemen_model'); 
+    } 
+ 
+    public function index() 
+    { 
+        
+		$data['listsurvei'] = $this->Survei_model->get_all(); 
+	    $this->load->view('template/head'); 
+	    $this->load->view('template/core_plugins'); 
+	    $this->load->view('kuantitatif/kerjasama', $data); 
+    } 
+ 
+
+ 
+    
+}
+
+ 
